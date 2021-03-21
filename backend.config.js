51e85682bdec441d97da2954a4a8446d7b245ba4
qalqi <qalqi@qalqi.com>
2021-03-21 12:30:10 +0530

    Update host

--- backend.config.js
+++ backend.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   postgresConnParams: {
     user: process.env.POSTGRES_USER || 'polkastats',
-    host: process.env.POSTGRES_HOST || 'localhost',
+    host: process.env.POSTGRES_HOST || 'postgres',
     database: process.env.POSTGRES_DATABASE || 'polkastats',
     password: process.env.POSTGRES_PASSWORD || 'polkastats',
     port: process.env.POSTGRES_PORT || 5432,
